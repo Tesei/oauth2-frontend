42b3038c10c9eaf6ee794a613ee376311e0ae71d
lootaas <d.ruzhin@fstravel.com>
2026-01-15 17:04:57 +0300

    add header and footer in  project

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import '@fun-sun/vue-components/vue-components.css'
 import '@fun-sun/vue-components/web-components.css'
 import '@fun-sun/style/dist/style.css'
 
+// Импорт стилей страниц (FSD)
+import './pages/login/styles.css'
+import './pages/signup/styles.css'
+
 // Экспорты Header и Footer
 export { HeaderBlock } from '@fun-sun/header'
 export { AppFooter } from '@fun-sun/footer'
