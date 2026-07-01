0e1e85b36be93be83664b660a8265ab0a53be9e7
Stepan Gudoshnikov <gud-stepan@yandex.ru>
2026-07-01 16:27:08 +0800

    deleted old- pages

--- src/index.ts
+++ src/index.ts
@@ -9,15 +9,6 @@ import './shared/config/back-button.css'
 // Импорт стилей страниц (FSD)
 import './pages/login/styles.css'
 import './pages/login-otp/styles.css'
-import './pages/old-login/styles.css'
-import './pages/old-signup/styles.css'
-import './pages/old-forgot-password/styles.css'
-import './pages/old-forgot-password-sent/styles.css'
-import './pages/old-reset-password/styles.css'
-import './pages/old-reset-password-success/styles.css'
-import './pages/old-reset-password-error/styles.css'
-import './pages/old-wb-auth-confirm/styles.css'
-import './pages/old-wb-auth-register/styles.css'
 
 // Экспорты директив
 export { vOutside } from '@fun-sun/ui-kit/directives'
